0c4253ab30bea75d69da696c5803d3228e78eee1
karimabdelehi <karimabdelhedi080@gmail.com>
2022-10-11 12:12:28 +0100

    done

---
+++ app.js
@@ -0,0 +1,99 @@
+
+$('body').css('background-color','Seashell') 
+ 
+//here I changed my background
+
+function badel33(){
+  $('#p1').fadeOut("slow")
+  $('#btn22').css('background-color','white')
+  $('#btn42').css('background-color','white')
+  $('#btn62').css('background-color','white')
+  $('#btn82').css('background-color','white')
+  $('#btn102').css('background-color','white')
+  $('#btn32').css('background-color','white')
+  $('#btn52').css('background-color','white')
+  $('#btn72').css('background-color','white')
+  $('#btn92').css('background-color','white')
+  $('#btn112').css('background-color','white')
+  } 
+  $('#btn').on('click',badel33)
+  
+
+
+ 
+
+ 
+ function changr(){ 
+   $('#btn22').css('background-color','green')
+ }
+ $('#btn22').onclick('click',changr)
+
+
+
+ function changr1(){ 
+   $('#btn42').css('background-color','green')
+ }
+ $('#btn42').onclick('click',changr)
+
+
+ function changr2(){ 
+   $('#btn62').css('background-color','green')
+ }
+ $('#btn62').onclick('click',changr1)
+
+
+ function changr3(){ 
+   $('#btn82').css('background-color','green')
+ }
+ $('#btn82').onclick('click',changr2)
+
+ 
+ function changr4(){ 
+   $('#btn102').css('background-color','green')
+ }
+ $('#btn102').onclick('click',changr3)
+
+ // IF you did your plan , you click for change the colore and confirmed 
+
+ function changr01(){ 
+  $('#btn32').css('background-color','red')
+}
+$('#btn32').onclick('click',changr)
+
+
+
+function changr11(){ 
+  $('#btn52').css('background-color','red')
+}
+$('#btn52').onclick('click',changr)
+
+
+function changr21(){ 
+  $('#btn72').css('background-color','red')
+}
+$('#btn72').onclick('click',changr1)
+
+
+function changr31(){ 
+  $('#btn92').css('background-color','red')
+}
+$('#btn92').onclick('click',changr2)
+
+
+function changr41(){ 
+  $('#btn112').css('background-color','red')
+}
+$('#btn112').onclick('click',changr3)
+
+
+// if you did not your plan , you click for change the colore and not confirmed 
+
+//********************************** */
+ 
+
+             
+
+         
+   
+
+  
---
+++ app1.js
@@ -0,0 +1,72 @@
+$('body').css('background-color','Seashell') 
+$('#p1').css('border','double')
+$('#img').attr('src','IMG_0979.JPG')
+ $('#img').css({'width': '140px', 'height': 'auto'})
+ $('#img1').attr('src','IMG_1190.JPG')
+ $('#img1').css({'width': '140px', 'height': 'auto'})
+ $('#img2').attr('src','IMG_E1167.JPG')
+ $('#img2').css({'width': '140px', 'height': 'auto'}) 
+
+ $('#img3').attr('src','IGJV4939.JPG')
+ $('#img3').css({'width': '140px', 'height': 'auto'})
+ $('#img4').attr('src','IMG_1305.JPG')
+ $('#img4').css({'width': '140px', 'height': 'auto'})
+ $('#img5').attr('src','IMG_E0836.JPG')
+ $('#img5').css({'width': '140px', 'height': 'auto'})
+
+ $('#img6').attr('src','IMG_E1024.JPG')
+ $('#img6').css({'width': '140px', 'height': 'auto'})
+ $('#img7').attr('src','IMG_E1858.JPG')
+ $('#img7').css({'width': '140px', 'height': 'auto'})
+ $('#img8').attr('src','IMG_E1968.JPG')
+ $('#img8').css({'width': '140px', 'height': 'auto'})
+
+ $('#img9').attr('src','IMG_E2085.JPG')
+ $('#img9').css({'width': '140px', 'height': 'auto'})
+ $('#img10').attr('src','MRKO6842.JPG')
+ $('#img10').css({'width': '140px', 'height': 'auto'})
+ $('#img11').attr('src','IMG_E1167.JPG')
+ $('#img11').css({'width': '140px', 'height': 'auto'})
+
+ // here a add my photos and fixed her css 
+
+ 
+
+function badel(){
+    $('#ul1').fadeOut("slow")
+    } 
+    $('#btn51').on('click',badel)
+    function badel1(){
+       $('#ul2').fadeOut("slow")
+       } 
+       $('#btn61').on('click',badel1)
+       function badel2(){
+          $('#ul3').fadeOut("slow")
+          } 
+          $('#btn71').on('click',badel2)
+          function badel3(){
+             $('#ul4').fadeOut("slow")
+             } 
+             $('#btn81').on('click',badel3)
+             // I hide my Photos  
+  
+  
+             function rajaa(){
+                $('#ul1').fadeIn("slow")
+             }
+             $('#btn11').on('click',rajaa)
+             function rajaa1(){
+                $('#ul2').fadeIn("slow")
+             }
+             $('#btn21').on('click',rajaa1)
+             function rajaa2(){
+                $('#ul3').fadeIn("slow")
+             }
+             $('#btn31').on('click',rajaa2)
+             function rajaa3(){
+                $('#ul4').fadeIn("slow")
+             }
+             $('#btn41').on('click',rajaa3)
+             // I show my photo 
+  
+  
